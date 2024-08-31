cb3fe2d8afce6e649420e15feebd5903be43c4da
Andre <andre.teizen@gmail.com>
2024-08-31 16:50:54 -0300

    Added meta tag for google SEO and fixed image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import Head from 'next/head'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -21,6 +22,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="kam8Jv2DTIOkoYIxIh90eMj5-ZskEGxqaY9dsNHyCgY"
+        />
+      </Head>
       <body className={poppins.className}>{children}</body>
     </html>
   )
