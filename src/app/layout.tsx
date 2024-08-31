9c9f5d9ed5d04cb831652de8b651636a5adf8ec8
Andre <andre.teizen@gmail.com>
2024-08-31 17:00:00 -0300

    Google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Head from 'next/head'
+import { Author } from 'next/dist/lib/metadata/types/metadata-types'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -10,9 +11,14 @@ const poppins = Poppins({
   display: 'swap',
 })
 
+const websiteAuthor = { name: 'André Teizen' } as Author
 export const metadata: Metadata = {
   title: 'Portifolio',
   description: 'Meu portifolio',
+  authors: websiteAuthor,
+  other: {
+    'google-site-verification': 'kam8Jv2DTIOkoYIxIh90eMj5-ZskEGxqaY9dsNHyCgY',
+  },
 }
 
 export default function RootLayout({
