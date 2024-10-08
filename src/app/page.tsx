3fc121f972a1186cea2a55163494a834266c891a
Andre <andre.teizen@gmail.com>
2024-10-08 08:27:01 -0300

    Ajuste description Portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
           printscreen={PortfolioPrint}
           title="Portfolio"
           shortDescription="I built this portfolio to seek opportunities and showcase all the tools I use or have used throughout my years as a web developer."
-          description={`Showcase your front-end expertise with a portfolio that highlights your ability to create intuitive and visually appealing web interfaces. Demonstrate your mastery of modern frameworks like React and Next.js, along with your proficiency in CSS and HTML. Let your portfolio be your digital resume, attracting potential employers and clients alike.`}
+          description={`Showcase my front-end expertise with a portfolio that highlights my ability to create intuitive and visually appealing web interfaces. Demonstrate my mastery of modern frameworks like Next.js and TailwindCSS, along with my proficiency in CSS and HTML.`}
           projectType="Fullstack"
           live="https://portifolio-teizen.vercel.app/"
           githubLink="https://github.com/andreteizen/Portifolio"
